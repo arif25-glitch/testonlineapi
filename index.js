8ca9e4076e2956d934b94cb5bd83772ef9e65c11
Arif Nur <nnarif37@gmail.com>
2022-10-02 09:50:49 +0700

    new web service api

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
 
-app.use(express.static("public"));
+app.use(express.static("frontend"));
 app.use(express.json({"limit" : "1mb"}));
 
-server.listen(port, () => console.log(`Listening`));
+server.listen(port, () => console.log("Listening"));
 
 app.get('/', (req, res) => {
-    res.end("ok this is fine")
+  if(!admin.apps.length){
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://fireapiproject-4f818-default-rtdb.asia-southeast1.firebasedatabase.app",
+      authDomain: "fireapiproject-4f818.firebaseapp.com"
+    });
+    
+    let database = admin.database();
+    let userRef = database.ref("users");
+  
+    userRef.once('value', (snapshot) => {
+    })
+    .then((result) => {
+      res.json(result.val());
+    })
+    .catch((err) => {
+      throw err;
+    })
+    .finally(() => {
+      admin.app().delete();
+    });
+  }
 })
